b9d0920b2fce510b1d8ba2cbd5723576b862c09f
Haim Bar <haim.bar@uconn.edu>
2022-08-06 09:51:03 -0400

    added a document on using a Makefile, fixed the troubleshooting

--- examples/Collaborate/code/MontyHall_2.R
+++
@@ -1,9 +0,0 @@
-countMTH = function(n, nds=3){
-    games = replicate(n, whichDoor(sample(1:3, 1), nds=nds))
-    mean(games[1,] == games[2,])
-    mean(games[3,] == games[2,])
-    return (c(mean(games[1,] == games[2,]), mean(games[3,] == games[2,])))
-    }
-
-# simulate 100 games to approximate the probabilities
-probabilities = paste(countMTH(100), collapse=", ")
