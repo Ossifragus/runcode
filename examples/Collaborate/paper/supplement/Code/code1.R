812ae76d6ef6924eba823794950c3ce49e59f71c
Haim Bar <haim.bar@uconn.edu>
2022-08-05 23:13:37 -0400

    deleted some redundant files in new example

--- examples/Collaborate/paper/supplement/Code/code1.R
+++
@@ -1,5 +0,0 @@
-set.seed(0) ## fix the random number
-x = rnorm(100)
-y = 1+x+rnorm(100)
-fit = lm(y~x)
-print(summary(fit))
--- examples/Collaborate/paper/supplement/Code/code2.R
+++
@@ -1,4 +0,0 @@
-library("xtable")
-fit.table <- xtable(aov(fit))
-MSE = format(sum(fit$residuals^2)/fit$df.residual, digit=2)
-print(fit.table)
