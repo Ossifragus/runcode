b337817c571e089872e46b02144076eb66248b8a
HaiYing Wang <whygps@gmail.com>
2022-08-20 20:44:56 -0400

    put ctan tarball into runcode folder

--- .ctan.sh
+++ .ctan.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-tar cpvfhz runcode.tar.gz runcode.sty runcode.tex runcode.pdf runcode_troubleshoot.tex runcode_troubleshoot.pdf README generated
+mkdir runcode
 
-# rm README header.tex 
+cp -r runcode.sty runcode.tex runcode.pdf runcode_troubleshoot.tex runcode_troubleshoot.pdf README generated runcode/
+
+tar cpvfhz runcode.tar.gz runcode/
+
+rm -r runcode 
 
 # tar cpvfhz runcode.tar.gz runcode.sty runcode.tex runcode.pdf troubleshoot.tex troubleshoot.pdf generated ./CTAN/README --transform='flags=r;s|./CTAN/README|README|'
 
